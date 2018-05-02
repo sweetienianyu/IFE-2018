84015571d771a00617e32d4a112ffc1483e188ad
sweetienianyu <475493955@qq.com>
2018-05-02 22:27:08 +0800

    add

---
+++ index.js
@@ -0,0 +1,14 @@
+import san, { Component } from 'san';
+var MyApp = san.defineComponent({
+    template: '<p>Hello {{name}}!</p>',
+
+    initData: function () {
+        return {
+            name: 'San'
+        };
+    }
+});
+
+
+var myApp = new MyApp();
+myApp.attach(document.body);
